ba8af0cca3bbbee32c9248937ca2a5048b618a67
kiprotichcollins254 <kiprotichcollins6191@gmail.com>
2023-05-12 10:55:03 +0300

    added angular js and made changes to first angular js

---
+++ app.js
@@ -0,0 +1,11 @@
+(()=>{
+  'use strict';
+
+  angular.module('MyFirstApp',[])
+  .controller('MyFirstController',($scope)=>{
+    // scope is a key word
+    $scope.name = "Customer"
+
+  });
+
+})();
